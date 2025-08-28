977252f4b8b6ab0356c351c1e0d0f279cd08c807
Cytus1 <xyloaex@gmail.com>
2025-08-28 15:19:49 -0400

    Did linked list, where communication of nodes is central base on memory rather than object definition. ( didn't do object.next, but -> as constructed a pointer with assigned address

--- Data Structure/linked lists.c
+++ Data Structure/linked lists.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 
 
+struct linkedlist {
+    int number;
+    struct Node* next;
+};
 
-int main(void){
-    
+
+
+int main(void) {
+    struct linkedlist node1;
+    struct linkedlist node2;
+    struct linkedlist node3;
+
+    node1.number = 1;
+    node2.number = 2;
+    node3.number = 3;
+
+    node1.next = &node2;
+    node2.next = &node3;
+    node3.next = NULL;
+
+    struct linkedlist* current = &node1;
+    while (current != NULL) {
+        printf("%d\n", current -> number);
+        current = current -> next;
+    }
 
     return 0;
 }
+
+// note: i made this linked list where the node was access through memory, not object, to show -> is useful in this case.
