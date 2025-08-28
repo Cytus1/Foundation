2373213e9508ec9be073a8e4b74f42a6c72c6bba
Cytus1 <xyloaex@gmail.com>
2025-08-28 15:36:51 -0400

    idea on graph in C, finish up in future.

---
+++ Data Structure/graphs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+// new idea: pointer to a pointer, but not enough knowledge yet to complete
+
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Graph {
+    int numVertices;
+    struct Node** adjLists;
+};
+
+
+int main(void){
+
+
+
+    return 0;
+}
