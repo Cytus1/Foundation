ca8544cd1e661ce1b7e102e99040fd7e25fea759
Cytus1 <xyloaex@gmail.com>
2025-09-24 13:09:17 -0400

    implemented pop (by index), and change isEmpty and isFull to boolean, with prinf to other function to fix repetitive print error

--- Data Structure/Beginner/stacks.c
+++ Data Structure/Beginner/stacks.c
@@ -15,25 +15,30 @@ struct Stack* createStack() {
     return Stack;
 }
 
-bool isEmpty(struct Stack* Stack){
-    printf("the stack is empty");
-    return Stack -> top == -1;
-}
-
-bool isFull(struct Stack* Stack){
-    printf("the stack is full");
-    return Stack -> top == MAXSIZE - 1;
-}
+bool isEmpty(struct Stack* s) { return s->top == -1; }
+bool isFull (struct Stack* s) { return s->top == MAXSIZE - 1; }
 
 void push(struct Stack* Stack, int data){
-    if (isFull (Stack)) {return;}
+    if (isFull (Stack)) {printf("Full stack\n"); return;}
     //Note: can't do Stack -> top = Stack -> top++;
     Stack -> top++;
     Stack -> items[Stack -> top] = data;
 }
 
+/*
+I realize that you can pop by removal, but there is no need to;
+You can overwrite them automatically each time you push anyways.
+*/
+void pop(struct Stack* Stack){
+    if (isEmpty (Stack)) {printf("Empty stack\n"); return;}
+    // Stack -> items[Stack -> top] = NULL;
+    int value = Stack -> items[Stack -> top];
+    printf("Popped: %d\n", value);
+    Stack -> top--;
+}
+
 void peek(struct Stack* Stack){
-    if      (isEmpty(Stack)) {return;}
+    if      (isEmpty(Stack)) {printf("Empty stack\n"); return;}
     printf("The top data is : %d\n", Stack -> items[Stack -> top]);
 }
 
@@ -43,10 +48,20 @@ int main (void){
 
     struct Stack* stack = createStack();
 
+    // testing empty
+    pop(stack);
+
     for (int i = 0; i < 5; i++){
         push(stack, i);
         peek(stack);
     }
+
+    // testing overflow
+    push(stack, 1);
+
+    // testing remove ; note: pop is not done by removal but index
+    pop(stack);
+    peek(stack);
 
     free(stack);
     return 0;
